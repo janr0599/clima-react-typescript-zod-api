fc226b913623a576f64eaa1c8988f5e37a253ffe
janr0599 <javiernr0599@gmail.com>
2024-09-28 11:49:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={styles.title}>Weather App</div>
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
